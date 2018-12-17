059ed8ddebdff50ee2521dc11cf9df637d644602
SeoulStore <hdcho@SeoulStoreui-MacBook-Pro.local>
2018-12-17 15:31:36 +0900

    Styling the To Do Component

--- ToDo.js
+++ ToDo.js
@@ -9,23 +9,59 @@ export default class ToDo extends Component {
         isCompleted: false
     }
     render(){
-        const { isCompleted } = this.state;
+        const { isCompleted, isEditing } = this.state;
         return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={this._toggleComplete}>
+          <View style={styles.column}>
+          <TouchableOpacity onPress={this._toggleComplete}>
                 <View style={[styles.circle, 
                               isCompleted ? styles.completedCircle : styles.uncompletedCircle
                               ]} />
             </TouchableOpacity>
-            <Text style={styles.text}>Hello I'm whoami </Text>
-        </View>
-        )
+            <Text style={[styles.text, isCompleted ? styles.completedText: styles.uncompletedText ]}>Hello I'm whoami </Text>
+          </View>
+
+              {isEditing ? ( 
+              <View style={styles.actions}>
+                    <TouchableOpacity onPressOut={this._finishEditing}>
+                        <View style={styles.actionContainer}>
+                            <Text style={styles.actionText}>✅</Text>
+                        </View>
+                    </TouchableOpacity>
+                 </View>   
+                ) : 
+                (
+                <View style={styles.actions}>
+                <TouchableOpacity onPressOut={this._startEditing}>
+                        <View style={styles.actionContainer}>
+                            <Text style={styles.actionText}>✏️</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <View style={styles.actionContainer}>
+                            <Text style={styles.actionText}>❌</Text>
+                        </View>
+                    </TouchableOpacity>
+                    </View>
+                )} 
+                </View>
+        );
     }
     _toggleComplete = () => {
         this.setState(prevState => {
             return {
                 isCompleted: !prevState.isCompleted
             }
+        })
+    }
+    _startEditing = () => {
+        this.setState({
+            isEditing: true
+        })
+    }
+    _finishEditing = () => {
+        this.setState({
+            isEditing: false
         })
     }
 }
@@ -35,7 +71,8 @@ const styles = StyleSheet.create({
         borderBottomColor:"#bbb",
         borderBottomWidth: StyleSheet.hairlineWidth,
         flexDirection: "row",
-        alignItems: "center"
+        alignItems: "center",
+        justifyContent:"space-between"
     },
     circle: {
         width: 30,
@@ -55,5 +92,25 @@ const styles = StyleSheet.create({
         fontWeight: "600",
         fontSize: 20, 
         marginVertical: 20 
+    },
+    completedText : {
+        color:"#bbb",
+        textDecorationLine: "line-through"
+    },
+    uncompletedText: {
+        color:"#353839"
+    },
+    column: {
+        flexDirection: "row",
+        alignItems : "center",
+        width : width / 2,
+        justifyContent: "space-between"
+    },
+    actions: {
+        flexDirection: "row"
+    },
+    actionContainer: {
+        marginVertical: 10,
+        marginHorizontal: 10
     }
 });
