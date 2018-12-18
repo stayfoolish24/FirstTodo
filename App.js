d56992ed5918aa2666a477a87638a38458abf398
SeoulStore <hdcho@SeoulStoreui-MacBook-Pro.local>
2018-12-18 14:56:10 +0900

    할일 목록 저장 , 삭제

--- App.js
+++ App.js
@@ -6,39 +6,56 @@
  * @flow
  */
 
-import React, {Component} from 'react'
-import {Platform, StyleSheet, Text, View, StatusBar, TextInput, Dimensions, ScrollView } from 'react-native'
+import React, { Component } from "react"
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  ScrollView
+} from "react-native"
 import ToDo from "./ToDo"
+import uuidv1 from "uuid/v1"
 
 const { height, width } = Dimensions.get("window")
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 })
-
 
 export default class App extends Component {
   state = {
-    newToDo: ""
-    }
+    newToDo: "",
+    toDos: {}
+  }
   render() {
-    const { newToDo } = this.state
+    const { newToDo, toDos } = this.state
     return (
       <View style={styles.container}>
         <StatusBar barstyle="light-content" />
         <Text style={styles.title}>First To Do </Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"} value={newToDo} onChangeText={this._contollNewToDo}
+          <TextInput
+            style={styles.input}
+            placeholder={"New To Do"}
+            value={newToDo}
+            onChangeText={this._contollNewToDo}
             placeholderTextColor={"#999"}
             returnKeyType={"done"}
             autoCorrect={false}
-           />
-           <ScrollView contentContainerStyle={styles.toDos}>
-            <ToDo text={"Hello I'm a To Do"} />
-           </ScrollView>
+            onSubmitEditing={this._addToDo}
+          />
+          <ScrollView contentContainerStyle={styles.toDos}>
+            {Object.values(toDos).map(toDo => (
+              <ToDo key={toDo.id} deleteToDo={this._deleteToDo} {...toDo} />
+            ))}
+          </ScrollView>
         </View>
       </View>
     )
@@ -48,24 +65,59 @@ export default class App extends Component {
       newToDo: text
     })
   }
-
+  _addToDo = () => {
+    const { newToDo } = this.state
+    if (newToDo !== "") {
+      this.setState(prevState => {
+        const ID = uuidv1()
+        const newToDoObject = {
+          [ID]: {
+            id: ID,
+            isCompleted: false,
+            text: newToDo,
+            createdAt: Date.now()
+          }
+        }
+        const newState = {
+          ...prevState,
+          newToDo: "",
+          toDos: {
+            ...prevState.toDos,
+            ...newToDoObject
+          }
+        }
+        return { ...newState }
+      })
+    }
+  }
+  _deleteToDo = id => {
+    this.setState(prevState => {
+      const toDos = prevState.toDos
+      delete toDos[id]
+      const newState = {
+        ...prevState,
+        ...toDos
+      }
+      return { ...newState }
+    })
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#FBF76A',
+    alignItems: "center",
+    backgroundColor: "#FBF76A"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
   },
   title: {
     color: "black",
@@ -81,28 +133,27 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
     ...Platform.select({
-       ios: {
-         shadowColor: "rgb(50, 50, 50)",
-         shadowOpacity: 0.5,
-         shadowRadius: 5,
-         shadowOffset: {
-           height:-1,
-           width:0 
-         }
-       },
-       android: {
-         elevation: 3
-       }
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
     })
   },
   input: {
     padding: 20,
     borderBottomColor: "#bbb",
     borderBottomWidth: 1,
-    fontSize: 25,
-
+    fontSize: 25
   },
   toDos: {
-    alignItems:"center"
+    alignItems: "center"
   }
 })
--- ToDo.js
+++ ToDo.js
@@ -1,27 +1,46 @@
 import React, { Component } from "react"
-import { View, Text, TouchableOpacity, StyleSheet, Dimensions, TextInput } from "react-native"
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  TextInput
+} from "react-native"
+import PropTypes from "prop-types"
 
-const {width, height } = Dimensions.get("window")
+const { width, height } = Dimensions.get("window")
 
 export default class ToDo extends Component {
-    state = {
+  constructor(props) {
+    super(props)
+    this.state = {
       isEditing: false,
-      isCompleted: false,
-      toDoValue: ""
+      toDoValue: props.text
     }
-    render(){
-      const { isCompleted,  isEditing, toDoValue } = this.state
-      const { text } = this.props
-        return (
-        <View style={styles.container}>
-          <View style={styles.column}>
-            <TouchableOpacity onPress={this._toggleComplete}>
-                <View style={[styles.circle, 
-                              isCompleted ? styles.completedCircle : styles.uncompletedCircle
-                              ]} 
-                />
-            </TouchableOpacity>
-            {isEditing ? (
+  }
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    isCompleted: PropTypes.bool.isRequired,
+    deleteToDo: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired
+  }
+
+  render() {
+    const { isCompleted, isEditing, toDoValue } = this.state
+    const { text, id, deleteToDo } = this.props
+    return (
+      <View style={styles.container}>
+        <View style={styles.column}>
+          <TouchableOpacity onPress={this._toggleComplete}>
+            <View
+              style={[
+                styles.circle,
+                isCompleted ? styles.completedCircle : styles.uncompletedCircle
+              ]}
+            />
+          </TouchableOpacity>
+          {isEditing ? (
             <TextInput
               style={[
                 styles.input,
@@ -44,9 +63,9 @@ export default class ToDo extends Component {
               {text}
             </Text>
           )}
-          </View>
+        </View>
 
-          {isEditing ? (
+        {isEditing ? (
           <View style={styles.actions}>
             <TouchableOpacity onPressOut={this._finishEditing}>
               <View style={styles.actionContainer}>
@@ -61,88 +80,86 @@ export default class ToDo extends Component {
                 <Text style={styles.actionText}>✏️</Text>
               </View>
             </TouchableOpacity>
-            <TouchableOpacity>
+            <TouchableOpacity onPressOut={() => deleteToDo(id)}>
               <View style={styles.actionContainer}>
                 <Text style={styles.actionText}>❌</Text>
               </View>
             </TouchableOpacity>
           </View>
         )}
-                </View>
-        )
-    }
-    _toggleComplete = () => {
-        this.setState(prevState => {
-            return {
-                isCompleted: !prevState.isCompleted
-            }
-        })
-    }
-    _startEditing = () => {
-        const { text } = this.props;
-        this.setState({ isEditing: true, toDoValue: text })
+      </View>
+    )
+  }
+  _toggleComplete = () => {
+    this.setState(prevState => {
+      return {
+        isCompleted: !prevState.isCompleted
       }
+    })
+  }
+  _startEditing = () => {
+    this.setState({ isEditing: true })
+  }
 
-    _finishEditing = () => {
+  _finishEditing = () => {
     this.setState({
       isEditing: false
     })
   }
-    _controllInput = text => {
+  _controllInput = text => {
     this.setState({ toDoValue: text })
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: width - 50,
-        borderBottomColor:"#bbb",
-        borderBottomWidth: StyleSheet.hairlineWidth,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent:"space-between"
-    },
-    circle: {
-        width: 30,
-        height: 30,
-        borderRadius: 15,
-        // borderColor: "#D3DA01",
-        borderWidth: 3,
-        marginRight: 20
-    },
-    completedCircle:   {
-        borderColor:"#bbb"
-    },
-    uncompletedCircle: {
-        borderColor:"#D3DA01"
-    },
-    text: {
-        fontWeight: "600",
-        fontSize: 20, 
-        marginVertical: 20 
-    },
-    completedText : {
-        color:"#bbb",
-        textDecorationLine: "line-through"
-    },
-    uncompletedText: {
-        color:"#353839"
-    },
-    column: {
-        flexDirection: "row",
-        alignItems : "center",
-        width : width / 2,
-        justifyContent: "space-between"
-    },
-    actions: {
-        flexDirection: "row"
-    },
-    actionContainer: {
-        marginVertical: 10,
-        marginHorizontal: 10
-    },
-    input: {
-        marginVertical: 15,
-        width: width / 2
-    }
+  container: {
+    width: width - 50,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    // borderColor: "#D3DA01",
+    borderWidth: 3,
+    marginRight: 20
+  },
+  completedCircle: {
+    borderColor: "#bbb"
+  },
+  uncompletedCircle: {
+    borderColor: "#D3DA01"
+  },
+  text: {
+    fontWeight: "600",
+    fontSize: 20,
+    marginVertical: 20
+  },
+  completedText: {
+    color: "#bbb",
+    textDecorationLine: "line-through"
+  },
+  uncompletedText: {
+    color: "#353839"
+  },
+  column: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: width / 2
+  },
+  actions: {
+    flexDirection: "row"
+  },
+  actionContainer: {
+    marginVertical: 10,
+    marginHorizontal: 10
+  },
+  input: {
+    marginVertical: 15,
+    width: width / 2
+  }
 })
